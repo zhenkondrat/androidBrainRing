c55643b34444cd28149e79ebcee44ab7aea3b459
zhenkondrat <zhenkondrat@meta.ua>
2015-07-03 00:12:04 +0300

    create class client for server

---
+++ BrainRingApp/app/src/main/java/com/example/zhenkondrat/brainringapp/Data/Client.java
@@ -0,0 +1,15 @@
+package com.example.zhenkondrat.brainringapp.Data;
+
+/**
+ * Created by zhEnkondrat on 03.07.2015.
+ */
+public class Client {
+    private String name;
+    private String ip;
+    private int zayavka;
+
+    public Client()
+    {
+        name="None";
+    }
+}
--- BrainRingApp/app/src/main/java/com/example/zhenkondrat/brainringapp/Data/PublicData.java
+++ BrainRingApp/app/src/main/java/com/example/zhenkondrat/brainringapp/Data/PublicData.java
@@ -11,11 +11,13 @@ public class PublicData {
     public static ArrayList<Round> rounds = new ArrayList<Round>();
     public static ArrayList<Member> members = new ArrayList<Member>();
     public static Leader leader = new Leader("None");
+    public static ArrayList<Client> clients = new ArrayList<Client>();
 
     public static void writeLog()
     {
         Log.v("publicData", "-=-=-");
         Log.v("Leader", leader.getGameName());
         Log.v("CountRounD", String.valueOf( rounds.size()));
+        Log.v("CountClient", String.valueOf( clients.size()));
     }
 }
